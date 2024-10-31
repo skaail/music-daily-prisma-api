fdc0b7572c8c3a7b21f30562405bf6060daaec37
Bruno Gianini <blgianini@gmail.com>
2024-10-31 00:11:50 -0300

    bugfix: cors

--- index.ts
+++ index.ts
@@ -4,7 +4,8 @@ import { routes } from './routes/routes'
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
 import { createServer } from 'http'
-import * as socketio from "socket.io";
+import * as socketio from "socket.io"
+import cors from 'cors'
 
 const prisma = new PrismaClient()
 const app = express()
@@ -13,6 +14,17 @@ const httpServer = createServer(app)
 app.use(bodyParser.json({ limit: '50mb', type: 'application/json' }))
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 dotenv.config()
+
+app.use(cors());
+
+const corsOptions = {
+  origin: '*', 
+  methods: 'GET,POST,PUT,DELETE, PATCH',      
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials: true,                         
+};
+
+app.use(cors(corsOptions));
 
 export const io = new socketio.Server(httpServer);
 
